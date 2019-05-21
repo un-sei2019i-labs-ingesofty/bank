76c423ce2556618d9eda6aa59877c195aaefaa87
Estudiante <Estudiante@BE454S404C22ING.unal.loc>
2019-05-21 15:48:10 -0500

    programita

---
+++ program.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+using namespace std;
+
+void funcionLaura(){
+
+}
+
+void funcionValentina(){
+
+}
+
+void funcionDaniel(){
+
+}
+
+int main(){
+	funcionLaura();
+	funcionValentina();
+	funcionDaniel();
+}
