f9af692ea369a1fc3ae7d06d6da6489afcabb2eb
mariavmflorez <mmolinaf@unal.edu.co>
2019-05-22 22:33:38 -0500

    Funcion Valentina

--- test/program.c
+++ test/program.c
@@ -7,7 +7,20 @@ void funcionLaura(){
 
 }
 
-void funcionValentina(){
+void funcionValentina(int n, int m){
+
+if (n>m){
+ 
+	cout<< "El numero mayor es : "<<n<<endl;
+}
+	else if (n==m)
+{
+	cout<< "Los numeros son iguales."<<n<<endl;
+	
+} 	else 
+
+	cout<< "El numero mayor es : "<<m<<endl;
+
 
 }
 
