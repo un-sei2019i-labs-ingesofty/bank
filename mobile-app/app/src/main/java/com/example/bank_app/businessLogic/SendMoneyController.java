356e1627489b11d3f516572837c085755d558bf3
juan <damorenor@unal.edu.co>
2019-06-11 00:06:33 -0500

    finished

--- mobile-app/app/src/main/java/com/example/bank_app/businessLogic/SendMoneyController.java
+++ mobile-app/app/src/main/java/com/example/bank_app/businessLogic/SendMoneyController.java
@@ -8,23 +8,24 @@ import com.example.bank_app.dataAccess.models.Account;
 import com.example.bank_app.dataAccess.models.User;
 
 import static com.example.bank_app.dataAccess.repositories.AccountRepository.getAccount;
+import static com.example.bank_app.dataAccess.repositories.AccountRepository.getAccountFromUser;
 import static com.example.bank_app.dataAccess.repositories.AccountRepository.modifyAccount;
 
 public class SendMoneyController {
-    public static void sendMoney(Context context, int accountNumber, float balance, User usuario){
+    public static boolean sendMoney(Context context, int accountNumber, float balance, User usuario){
 
         Account acc2 = getAccount(context, accountNumber);
         Account acc1 = getAccountFromUser(context, usuario);
-        if(balance> 0 ) {
+        if(acc2.getAccountNumber()==0){
+            return false;//error()
+        }
+        if(acc1.getBalance()> balance ) {
             modifyAccount(context, acc2);
             modifyAccount(context, acc1);
+            return true;
+        }else {
+            return false;//error()
         }
-        
-
-
-
-
-
 
     }
 }
